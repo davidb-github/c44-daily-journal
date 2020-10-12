404025a7b6af34ef2b123009474d037fe198a9af
David Bing <davidb.devops@gmail.com>
2020-10-12 09:59:51 -0500

    update JournalEntry.js: delete unwanted text from return html

--- scripts/JournalEntry.js
+++ scripts/JournalEntry.js
@@ -6,9 +6,7 @@ export const JournalEntryComponent = (entry) => {
     return `
         <section id="entry--${entry.id}" class="journalEntry">
         <div>${entry.date}</div>
-        <div>${entry.entry}</div>
-        Display the entry's full text, and the date
-            it was entered here.
+        <div>${entry.entry}</div><br>
         </section>
     `
 }
