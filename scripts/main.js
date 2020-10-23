da042cf76e3875708261915d3f60aa735caf1c92
David Bing <davidb.devops@gmail.com>
2020-10-23 10:05:51 -0500

    update module: main.js - improve commenting

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,8 @@
+// import components in external modules
 import { EntryListComponent } from './JournalEntryList.js'
 
+// test fire to ensure main.js is wired correctly
 console.log("Welcome to the main module")
 
+// invoke functions
 EntryListComponent();
